d92e63c7e4ce0928b0da81ec1abeea2a5b7b6112
KARANVEERMANOHAR <74814835+KARANVEERMANOHAR@users.noreply.github.com>
2022-09-20 11:02:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [data, setData] = useState("");
+  const showData = () => {
+    setData(
+      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+    );
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      <button
+        id="click "
+        onClick={() => {
+          showData();
+        }}
+      >
+        Click
+      </button>
+      <p id="para">{data}</p>
     </div>
   );
 }
-
 
 export default App;
