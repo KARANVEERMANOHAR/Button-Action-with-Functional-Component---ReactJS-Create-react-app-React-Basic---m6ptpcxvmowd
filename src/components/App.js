59412c879fccb186802174d330fdba525ef4e64e
Karanveer Manohar <karanveermanohar30@gmail.com>
2022-09-26 06:04:41 +0000

    Update package-lock.json and App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,27 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [data, setData] = useState("");
+  const [para, setPara] = useState(false);
   const showData = () => {
-    setData(
-      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
-    );
+    setPara(true);
   };
   return (
     <div id="main">
       <button
-        id="click "
+        id="click"
         onClick={() => {
           showData();
         }}
       >
         Click
       </button>
-      <p id="para">{data}</p>
+
+      {para && (
+        <p id="para">
+          "Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy"
+        </p>
+      )}
     </div>
   );
 }
